1097756c1e130bdd353af22e38c5036b1ed06a21
Kyle Truong <kykyleb40@gmail.com>
2026-05-15 13:29:44 -0400

    N/A

--- backup_sync/include/Logger.hpp
+++ backup_sync/include/Logger.hpp
@@ -2,8 +2,9 @@
 #include <mutex>
 
 class Logger {
-    public:
-        static void log(const std::string& message)
-    private:
-        static std::mutex logMutex;
+public:
+    static void log(const std::string& message);
+
+private:
+    static std::mutex logMutex;
 };
