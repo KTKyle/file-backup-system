6150b6b7244f0cc880e6556bb8e554773ffe9ac3
Kyle Truong <kykyleb40@gmail.com>
2026-05-15 13:29:29 -0400

    basic main file setup

--- backup_sync/src/main.cpp
+++ backup_sync/src/main.cpp
@@ -4,8 +4,30 @@ Parse CLI arguments
 Initialize system
 Start backup process
 */
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "../include/FileCopier.hpp"
+#include "../include/FileScanner.hpp"
+#include "../include/Logger.hpp"
+#include "../include/ThreadPool.hpp"
+#include "../include/Utils.hpp"
 
 int main(int argc, char* argv[])
 {
-    
+    if(argc != 4) // Verifies number of cli args
+    {
+        std::cout << "Error: Wrong number of arguments!" << std::endl;
+        std::cout << "Usage: ./backup_tool <source_dir> <backup_dir> <thread_count>" << std::endl;
+        return 1;
+    }
+    std::string sourceDir = argv[1];
+    std::string backupDir = argv[2]; 
+    int threadCount = std::stoi(argv[3]);
+
+    std::cout << "Source Directory: " << sourceDir << std::endl;
+    std::cout << "Backup Directory: " << backupDir << std::endl;
+    std::cout << "Thread Count: " << threadCount << std::endl;
+    return 0;
 }
