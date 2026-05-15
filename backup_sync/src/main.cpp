afb606fc1eca0af3c0b75aa31b129e27cdc65264
Kyle Truong <kykyleb40@gmail.com>
2026-05-15 16:17:05 -0400

    Implemented main, temination errors need to fix

--- backup_sync/src/main.cpp
+++ backup_sync/src/main.cpp
@@ -22,15 +22,44 @@ int main(int argc, char* argv[])
         std::cout << "Usage: ./backup_tool <source_dir> <backup_dir> <thread_count>" << std::endl;
         return 1;
     }
-    std::string sourceDir = argv[1];
-    std::string backupDir = argv[2]; 
+    std::filesystem::path sourceDir = argv[1];
+    std::filesystem::path backupDir = argv[2];
     int threadCount = std::stoi(argv[3]);
 
     std::cout << "Source Directory: " << sourceDir << std::endl;
     std::cout << "Backup Directory: " << backupDir << std::endl;
     std::cout << "Thread Count: " << threadCount << std::endl;
 
+    FileScanner scanner;
+    std::vector<std::filesystem::path> files = scanner.scan(sourceDir);
 
-    
+    if(files.empty())
+    {
+        std::cout << "No Files found.\n";
+        return 0;
+    }
+
+    ThreadPool pool(threadCount);
+    pool.start();
+
+    for(const auto& file : files)
+    {
+        if(!Utils::fileExists(file))
+        {
+            continue;
+        }
+
+        std::filesystem::path relative = Utils::makeRelativePath(sourceDir, file);
+        std::filesystem::path dest = std::filesystem::path(backupDir) / relative;
+
+        Job job;
+        job.source = file;
+        job.destination = dest;
+
+        pool.addJob(job);
+    }
+
+    pool.wait();
+    std::cout << "Backup Complete.\n";
     return 0;
 }
